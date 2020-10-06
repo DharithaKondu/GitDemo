becf723ebfc2b6e0e47dfa581542db63b742f7f2
dharitha <dharithareddy.441@gmail.com>
2020-10-06 15:03:01 +0530

    American guy commit

---
+++ src/test/java/qaclickacademy/Mavenjava/RestApiTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+	
+	
+	@Test
+	public void postJira() {
+		System.out.println("postJira");
+		System.out.println("postJira4");
+		System.out.println("postJira5");
+		System.out.println("postJira6");
+	}
+	
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deletetwitter");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/SeleniumTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void BrowserAutomation()
+	
+	{
+		System.out.println("BrowserAutomation");
+		System.out.println("WebAutomation");
+		
+	}
+	
+	@Test
+	public void ElementsUi() {
+		System.out.println("elementsui");
+	}
+
+}
