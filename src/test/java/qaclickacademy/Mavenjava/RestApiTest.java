a86beb24749fa2b0c8a8f32765ea3f29c05832ac
dharitha <dharithareddy.441@gmail.com>
2020-10-06 14:09:45 +0530

    this is commit from Asian Time zone guy

---
+++ src/test/java/qaclickacademy/Mavenjava/RestApiTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+	
+	
+	@Test
+	public void postJira() {
+		System.out.println("postJira");
+		System.out.println("postJira2");
+		System.out.println("postJira3");
+	}
+	
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deletetwitter");
+	}
+
+}
