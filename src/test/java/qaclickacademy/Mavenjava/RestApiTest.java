51efc3b1ac2215fc745dd8f13d9b1b97dd61d24f
dharitha <dharithareddy.441@gmail.com>
2020-10-06 15:42:14 +0530

    time

--- src/test/java/qaclickacademy/Mavenjava/RestApiTest.java
+++ src/test/java/qaclickacademy/Mavenjava/RestApiTest.java
@@ -8,9 +8,13 @@ public class RestApiTest {
 	@Test
 	public void postJira() {
 		System.out.println("postJira");
+
 		System.out.println("postJira4");
 		System.out.println("postJira5");
 		System.out.println("postJira6");
+		System.out.println("postJira2");
+		System.out.println("postJira3");
+
 	}
 	
 	@Test
